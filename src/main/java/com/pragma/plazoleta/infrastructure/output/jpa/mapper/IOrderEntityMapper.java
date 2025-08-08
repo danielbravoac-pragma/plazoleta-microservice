82223d0712ba26b77e8029e4aee35918ae7a3b83
Daniel Bravo <daniel.bravo@pragma.com.co>
2025-08-07 22:41:34 -0500

    feat: correction save and list order

--- src/main/java/com/pragma/plazoleta/infrastructure/output/jpa/mapper/IOrderEntityMapper.java
+++ src/main/java/com/pragma/plazoleta/infrastructure/output/jpa/mapper/IOrderEntityMapper.java
@@ -2,13 +2,11 @@ package com.pragma.plazoleta.infrastructure.output.jpa.mapper;
 
 
 import com.pragma.plazoleta.domain.model.Order;
+import com.pragma.plazoleta.domain.model.OrderDetail;
 import com.pragma.plazoleta.domain.model.OrderStatus;
-import com.pragma.plazoleta.infrastructure.output.jpa.entity.OrderEntity;
-import com.pragma.plazoleta.infrastructure.output.jpa.entity.OrderStatusEntity;
-import com.pragma.plazoleta.infrastructure.output.jpa.entity.OrderStatusId;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
+import com.pragma.plazoleta.domain.model.Restaurant;
+import com.pragma.plazoleta.infrastructure.output.jpa.entity.*;
+import org.mapstruct.*;
 
 import java.util.List;
 
@@ -16,14 +14,31 @@ import java.util.List;
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface IOrderEntityMapper {
+
+    @Mapping(target = "details", source = "details")
     Order toOrder(OrderEntity orderEntity);
 
+    @Mapping(target = "details", source = "details")
     OrderEntity toOrderEntity(Order order);
 
     List<Order> toOrderList(List<OrderEntity> orderEntityList);
 
     @Mapping(target = "order", ignore = true)
+    OrderDetail toOrderDetail(OrderDetailEntity entity);
+
+    List<OrderDetail> orderDetailEntityListToOrderDetailList(List<OrderDetailEntity> list);
+
+
+    @Mapping(target = "order", ignore = true)
+    OrderDetailEntity toOrderDetailEntity(OrderDetail orderDetail);
+
+    List<OrderDetailEntity> orderDetailListToOrderDetailEntityList(List<OrderDetail> list);
+
+    @Mapping(target = "order", ignore = true)
     OrderStatusEntity orderStatusToOrderStatusEntity(OrderStatus status);
+
+    @Mapping(target = "dishes", ignore = true)
+    Restaurant restaurantEntityToRestaurant(RestaurantEntity restaurantEntity);
 
     default Long map(OrderStatusId id) {
         return id != null ? id.getStatusId() : null;
@@ -34,5 +49,12 @@ public interface IOrderEntityMapper {
         OrderStatusId orderStatusId = new OrderStatusId();
         orderStatusId.setStatusId(id);
         return orderStatusId;
+    }
+
+    @AfterMapping
+    default void setReverseRelations(@MappingTarget OrderEntity order) {
+        if (order.getDetails() != null) {
+            order.getDetails().forEach(d -> d.setOrder(order));
+        }
     }
 }
